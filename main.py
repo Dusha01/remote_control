7541c4e706a47916009eba2a899243065e0f1fd9
Dusha_01 <87571857+Dusha01@users.noreply.github.com>
2025-02-07 20:34:16 +1000

    download model
    working....

---
+++ main.py
@@ -0,0 +1,29 @@
+import cv2
+import mediapipe as mp
+
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands(max_num_hands=2, min_detection_confidence=0.7)
+mp_drawing = mp.solutions.drawing_utils
+
+cap = cv2.VideoCapture(0)
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    image_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    results = hands.process(image_rgb)
+
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+
+    cv2.imshow('window: ', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        exit()
+
+cap.release()
+cv2.destroyAllWindows()
