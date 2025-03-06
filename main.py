311a2eb064bc968dbfcaf032c23874f546ff90fb
Dusha_01 <87571857+Dusha01@users.noreply.github.com>
2025-03-06 19:44:52 +1000

    один жест +

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from math import sqrt
 import pycaw.pycaw as pycaw
 from comtypes import CLSCTX_ALL
 from ctypes import cast, POINTER
+import time
 
 #----------------------------------------------------------------------
 
@@ -25,16 +26,22 @@ roi_y = 10
 roi_width = 350
 roi_height = 350
 
+#========================================================================================================
+#1
 def find_dig(point_1, point_2):
     return sqrt((point_1[0]-point_2[0])**2 + (point_1[1]-point_2[1])**2)
 
-def detect_gesture(hand_landmarks, img_width, img_height):
+def detect_volume_gesture(hand_landmarks, img_width, img_height, min_distance=20, max_distance=150):
     tip_big_finger = hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP]
     tip_ukaz_finger = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP]
     big_x, big_y = int(tip_big_finger.x * img_width), int(tip_big_finger.y * img_height)
     ukaz_x, ukaz_y = int(tip_ukaz_finger.x * img_width), int(tip_ukaz_finger.y * img_height)
     distance = find_dig((big_x, big_y), (ukaz_x, ukaz_y))
-    return distance
+
+    if min_distance <= distance <= max_distance:
+        return distance
+    else:
+        return None
 
 def set_volume(distance, max_distance, min_distance):
     distance = max(min_distance, min(max_distance, distance))
@@ -44,15 +51,32 @@ def set_volume(distance, max_distance, min_distance):
     current_volume = volume.GetMasterVolumeLevel()
     return current_volume
 
+def detect_stop_one(hand_landmarks, img_width, img_height, threshold=0.07):
+    pinky_tip = hand_landmarks.landmark[mp_hands.HandLandmark.PINKY_TIP]
+    pinky_pip = hand_landmarks.landmark[mp_hands.HandLandmark.PINKY_PIP]
+    pinky_mcp = hand_landmarks.landmark[mp_hands.HandLandmark.PINKY_MCP]
+
+    tip_y = pinky_tip.y
+    pip_y = pinky_pip.y
+    mcp_y = pinky_mcp.y
+
+    if tip_y < pip_y and pip_y < mcp_y:
+        return True
+
+    return False
+#========================================================================================================
+
 min_distance = 20
 max_distance = 150
 previous_volume = None
-wrist_y_threshold = 30
-last_wrist_y = None
-volume_locked = False
-locked_volume = None
+volume_control_active = False
+
+gesture_recognition_time = 2
+gesture_start_time = None
+pinky_straight_start_time = None
 
 while cap.isOpened():
+    volume_text = "Volume: N/A"
     ret, frame = cap.read()
     if not ret:
         break
@@ -68,9 +92,9 @@ while cap.isOpened():
         for hand_landmarks in results.multi_hand_landmarks:
             is_inside_roi = False
             for landmark in hand_landmarks.landmark:
-                x = int(landmark.x * image_width) 
+                x = int(landmark.x * image_width)
                 y = int(landmark.y * image_height)
-                if 0 <= x <= image_width and 0 <= y <= image_height: 
+                if 0 <= x <= image_width and 0 <= y <= image_height:
                     is_inside_roi = True
                     break
             if is_inside_roi:
@@ -79,50 +103,64 @@ while cap.isOpened():
         for hand_landmarks in filtered_hand_landmarks:
             mp_drawing.draw_landmarks(roi, hand_landmarks, mp_hands.HAND_CONNECTIONS)
 
-            wrist = hand_landmarks.landmark[mp_hands.HandLandmark.WRIST]
-            wrist_y = int(wrist.y * image_height)
+            distance = detect_volume_gesture(hand_landmarks, image_width, image_height)
+            is_pinky_straight = detect_stop_one(hand_landmarks, image_width, image_height)
 
-            if last_wrist_y is None:
-                last_wrist_y = wrist_y
-            y_difference = wrist_y - last_wrist_y
+            if is_pinky_straight:
+                if pinky_straight_start_time is None:
+                    pinky_straight_start_time = time.time()
+                elif time.time() - pinky_straight_start_time >= 1.5:
+                    gesture_start_time = None
+                    volume_control_active = False
+                    pinky_straight_start_time = None
+                    volume_text = "Volume: mini_pr+"
+                    print("Volume: mini_pr-")
+                    continue
 
-            if y_difference > wrist_y_threshold and not volume_locked:
-                volume_locked = True
-                locked_volume = previous_volume
-                print("Volume locked (downward motion).")
-
-            elif y_difference < -wrist_y_threshold and volume_locked:
-                volume_locked = False
-                locked_volume = None
-                print("Volume unlocked (upward motion).")
-
-            text_color = (0, 255, 0)
-            if volume_locked:
-                text_color = (0, 0, 255)
-
-            if volume_locked and locked_volume is not None:
-                volume_percentage = int(((locked_volume - minVol) / (maxVol - minVol)) * 100)
-                volume_text = f"Volume: {volume_percentage}% (Locked)"
             else:
-                distance = detect_gesture(hand_landmarks, image_width, image_height)
-                if distance is not None:
+                pinky_straight_start_time = None
+
+
+            if distance is not None:
+                if gesture_start_time is None:
+                    gesture_start_time = time.time()
+                    print("Gesture started...")
+
+                elif time.time() - gesture_start_time >= gesture_recognition_time:
+                    volume_control_active = True
                     current_volume = set_volume(distance, max_distance, min_distance)
                     volume_percentage = int(((current_volume - minVol) / (maxVol - minVol)) * 100)
-                    volume_text = f"Volume: {volume_percentage}%"
+                    volume_text = f"Volume: {volume_percentage}% (Control Active)"
+                    previous_volume = current_volume
+                    print("Active_1")
+
+                elif volume_control_active:
+                    current_volume = set_volume(distance, max_distance, min_distance)
+                    volume_percentage = int(((current_volume - minVol) / (maxVol - minVol)) * 100)
+                    volume_text = f"Volume: {volume_percentage}% (Adjusting)"
+                    previous_volume = current_volume
+
+                else:
+                    volume_percentage = 0
+                    volume_text = f"Recognizing ({round(time.time()-gesture_start_time, 1)}/{gesture_recognition_time} sec)"
+
+
+            else:
+                gesture_start_time = None
+                if volume_control_active:
+                    volume_text = "Gesture lost."
                 else:
                     volume_text = "Volume: N/A"
 
+
+            text_color = (0, 255, 0)
+
             cv2.putText(roi, volume_text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, text_color, 2)
 
-            if not volume_locked:
-                distance = detect_gesture(hand_landmarks, image_width, image_height)
-
-                if distance is not None:
-                    current_volume = set_volume(distance, max_distance, min_distance)
-                    if previous_volume is None or abs(current_volume - previous_volume) > 0.5:
-                        previous_volume = current_volume
-
-            last_wrist_y = wrist_y
+    else:
+        gesture_start_time = None
+        volume_control_active = False
+        pinky_straight_start_time = None
 
     cv2.rectangle(frame, (roi_x, roi_y), (roi_x + roi_width, roi_y + roi_height), (255, 255, 255), 2)
 
